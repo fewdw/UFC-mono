241054afe5e461e75e56febb4f5c7bf6cfc51d73
fewdw <frederic.alefebvre@gmail.com>
2025-01-30 17:02:50 -0500

    first

---
+++ livescoring-service/src/main/java/com/ufc/livescoring/LivescoringApplication.java
@@ -0,0 +1,13 @@
+package com.ufc.livescoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivescoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivescoringApplication.class, args);
+	}
+
+}
